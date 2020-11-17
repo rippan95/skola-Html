8ec091461e98b78bedd529be6b8d15b6aba8bcfa
rippan95 <emmap9508@live.com>
2020-11-17 18:47:58 +0100

    KLAR 2

--- js/Uppgift-02.js
+++ js/Uppgift-02.js
@@ -1,3 +1,28 @@
 /* Biggest number */
 
-/* */
+/* Skriv en funktion som jämför två tal och retunerar det största*/
+
+/* steg 1 */ 
+function Biggest_number (x, y) {
+    if (x > y) {
+        return x;
+    } else {
+        return y;
+    }
+}
+let result = Biggest_number (2 ,4);
+
+console.log(result)
+
+
+/*exempel 2 */
+function Lowest_number (x, y) {
+    if (x < y) {
+        return x;
+    } else {
+        return y;
+    }
+}
+let result = Lowest_number (-6 ,4);
+
+console.log(result)
