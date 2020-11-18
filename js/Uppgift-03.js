beff7e6bb1bee1606999d382cd5e714190e9543e
rippan95 <emmap9508@live.com>
2020-11-18 10:44:00 +0100

    3 är klar

--- js/Uppgift-03.js
+++ js/Uppgift-03.js
@@ -5,3 +5,38 @@
 Skriv en annan funktion som tar två parametatra, start_year och end_year. och som skriver ut 
 varje år i intervallet och året är ett skottår eller inte*/
 
+function Leap_year (year) {
+    let divided_four   = (year % 4 ) === 0;
+    let divided_hundred = (year % 100 ) === 0;
+
+    //console.log (divided_four && ! divided_hundred );
+
+    if (divided_four && ! divided_hundred ) {
+        return true;
+       } else {
+        return false;
+    }
+}
+
+let result = leap_year ( 2000 );
+
+console.log ( result );
+
+// sista delen 
+
+function Leap_year (year) {
+    let divided_four   = (year % 4 ) === 0;
+    let divided_hundred = (year % 100 ) === 0;
+
+    return ( divided_four && ! divided_hundred );
+}
+
+function check_year (start_year, end_year) {
+    for (let year = start_year; year <= end_year; year++ ) {
+         let leap = leap_year ( year ) ? "skottår" : " ej skottår ";
+
+       console.log ( year + ": " + leap );
+    }
+}
+
+check_year (2015, 2025 );
---
+++ js/class.js
@@ -0,0 +1,10 @@
+class Student {
+  
+  greeting() {
+    console.log("Hello");
+  }
+
+}
+
+let studentOne = new Student();
+studentOne.greeting();
